1a53db75f1626afacf44b55989a783cc39434615
xXsheroXx <shero010807@gmail.com>
2025-01-19 21:39:34 +0100

    Added book cover and the latest books cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
   <>
-    <BookOverview />
+    <BookOverview {...sampleBooks[0]} />
 
-    <BookList />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
