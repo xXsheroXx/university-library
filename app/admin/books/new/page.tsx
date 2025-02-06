acdaaffd426e574ee0e7500f9590b5dcef4d4bbf
xXsheroXx <shero010807@gmail.com>
2025-02-06 17:04:32 +0100

    Completed the UI for the add book form

--- app/admin/books/new/page.tsx
+++ app/admin/books/new/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import BookForm from "@/components/admin/forms/BookForm";
 
 const Page = () => {
   return (
@@ -10,7 +11,7 @@ const Page = () => {
       </Button>
 
       <section className="w-full max-w-2xl">
-        <p>Book Form</p>
+        <BookForm />
       </section>
     </>
   );
